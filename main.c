6d649fbb0785bbdf3548041f03fb0c1ccef4a184
GameSpy <54669564+gabengaGamer@users.noreply.github.com>
2024-07-07 19:50:41 +0200

    Update 0.1
    Implementation of automatic search for vertices in HEX space

--- main.c
+++ main.c
@@ -38,6 +38,10 @@ int main(int argc, char *argv[])
     int voffs = 1;
 	unsigned short *pi;
 	unsigned short v1, v2, v3;
+	
+    int found = 0;
+    size_t vert_offset = 0;
+    unsigned char bytes[6];
 
 //=============================================================================
 // FILE SYSTEM
@@ -75,11 +79,25 @@ int main(int argc, char *argv[])
 //=============================================================================
 
 	fprintf(stderr, "Processing\n", argv[0]);
+	
+    while (fread(bytes, 1, 4, f_in) == 4) { //Finding beginning of vertices by pattern
+        if (bytes[0] == 0x00 &&
+            bytes[1] > 0x00 &&
+            bytes[2] > 0x00 &&
+            bytes[3] > 0x00) {
+            found = 1;
+            vert_offset += 1;
+            break;
+        }
+        fseek(f_in, -3, SEEK_CUR); //Back to the beginning
+        vert_offset++;
+    }
 
-	//fseek(f_in, 0x14, SEEK_SET);
+	fseek(f_in, 0x14, SEEK_SET); //Temp solution aka hacki the hack
 	fread(&vert_cnt, 1, sizeof(vert_cnt), f_in);
 	
-	//fseek(f_in, 0x2c, SEEK_SET);
+	//fseek(f_in, 0x2c, SEEK_SET); //Temp solution aka hacki the hack
+	fseek(f_in, vert_offset, SEEK_SET); //Setting vertex reading position
     verts = malloc(sizeof(struct MYVERTEX)*vert_cnt);
 	fread(verts, 1, sizeof(struct MYVERTEX)*vert_cnt, f_in);
 	
@@ -88,19 +106,20 @@ int main(int argc, char *argv[])
 		fprintf(f_out, "v %f %f %f\n", verts[i].pos[0], verts[i].pos[1], verts[i].pos[2]);
 	}
 	for(i = 0; i < vert_cnt; i++) {
-		verts[i].normal[0] = -verts[i].normal[0]; //Inverting normals to sync changes with vertices.
-		verts[i].normal[2] = -verts[i].normal[2];
+		verts[i].normal[0] = -verts[i].normal[0]; //Inverting X axis normals to sync changes with vertices.
+		verts[i].normal[2] = -verts[i].normal[2]; //Inverting Y axis normals to sync changes with vertices.
 		fprintf(f_out, "vn %f %f %f\n", verts[i].normal[0], verts[i].normal[1], verts[i].normal[2]); 
 	}
 	for(i = 0; i < vert_cnt; i++) {
-		verts[i].tc[1] = 1.0f -verts[i].tc[1]; //Inverting UVs to sync with texture maps.
+		verts[i].tc[1] = 1.0f -verts[i].tc[1]; //Inverting UV to sync with texture maps.
 		fprintf(f_out, "vt %f %f\n", verts[i].tc[0], verts[i].tc[1]); 
 	}
 
-	//fseek(f_in, 0x10, SEEK_SET);
+	fseek(f_in, 0x10, SEEK_SET); //Temp solution aka hacki the hack
 	fread(&index_cnt, 1, sizeof(index_cnt), f_in);
+	
 
-	//fseek(f_in, 0x78cc, SEEK_SET);
+	fseek(f_in, 0x78cc, SEEK_SET); //Temp solution aka hacki the hack
 	indices = malloc(index_cnt * sizeof(unsigned short));
 	fread(indices, 1, sizeof(unsigned short)*index_cnt, f_in);
 	
